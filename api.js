f5609b310115bb570e8a8d41f315a8dbd7f3b340
Isabek Tashiev <isabek2309@gmail.com>
2015-02-23 15:15:50 -0500

    Added "student taken lessons" API

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ var sessionStore = require("./lib/session-store");
 var handleAuthenticate = require("./lib/handle-authenticate");
 var hasAuthenticated = require("./lib/has-authenticated");
 var studentInfo = require("./lib/student-info");
+var studentTakenLessons = require("./lib/student-taken-lessons");
 
 var router = new Router();
 
@@ -34,9 +35,24 @@ router.get('/student-info', function (req, res) {
 
 router.get('/student-taken-lessons', function (req, res) {
 
-});
+    var authKey = req.body.authKey;
 
-router.get('/')
+    hasAuthenticated(sessionStore, authKey, function (err) {
+        if (err) {
+            res.end(JSON.stringify(err));
+        } else {
+            var jar = sessionStore.getJar(authKey);
+
+            studentTakenLessons(jar, function (err, lessons) {
+                if (err) {
+                    res.end(JSON.stringify(err));
+                } else {
+                    res.end(JSON.stringify(lessons));
+                }
+            });
+        }
+    });
+});
 
 var server = http.createServer(router);
 
