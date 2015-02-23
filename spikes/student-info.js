36c4ee0dd7621a69e6ae27cb0c343c49b34cf90d
Isabek Tashiev <isabek2309@gmail.com>
2015-02-23 16:02:41 -0500

    Added student transcript API, but not finished

--- spikes/student-info.js
+++ spikes/student-info.js
@@ -1,12 +1,11 @@
 var loginToOgrnot = require("../lib/login");
-var studentInfo = require("../lib/student-info");
-var studentTakenLessons = require("../lib/student-taken-lessons");
+var studentTranscript = require("../lib/student-transcript");
 
 loginToOgrnot("1004.01029", "aDi93", function (err, jar) {
     if (err) {
         console.log(err);
     }
-    studentTakenLessons(jar, function (err, studentInfo) {
+    studentTranscript(jar, function (err, studentInfo) {
         console.log(studentInfo);
     });
 });
