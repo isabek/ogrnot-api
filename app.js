b36823013c9a6b4e8bbbf18d202c7299ab362be4
Isabek <isabek2309@gmail.com>
2015-03-03 18:40:48 +0600

    Fixed

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var app = require("./api").app;
 var host = process.env.HOST || '0.0.0.0',
     port = process.env.PORT || 8080;
 
-app.listen(host, port, function () {
+app.listen(port, host, function () {
     console.log("Server running on %s:%d", host, port);
 });
