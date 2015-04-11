425e1ac69d6972040fb60a922feeb3d500c5c45e
--system <isabek2309@gmail.com>
2015-04-11 13:47:06 +0600

    Added uncaughtException handler

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var app = require("./api").app;
 var host = process.env.HOST || '0.0.0.0',
     port = process.env.PORT || 8080;
 
+process.on("uncaughtException", function (err) {
+    console.log(err);
+    console.trace(err);
+
+    setTimeout(process.end, 1000);
+});
+
 app.listen(port, host, function () {
     console.log("Server running on %s:%d", host, port);
 });
