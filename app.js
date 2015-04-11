0a72836f1fa4f26df135026d2c4bc5522b1ca549
--system <isabek2309@gmail.com>
2015-04-12 01:17:38 +0600

    Deleted htmltidy. Added htmltidy from my fork.

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ var app = require("./api").app;
 var host = process.env.HOST || '0.0.0.0',
     port = process.env.PORT || 8080;
 
-process.on("uncaughtException", function (err) {
-    console.log(err);
-    console.trace(err);
-
-    setTimeout(process.end, 1000);
-});
-
 app.listen(port, host, function () {
     console.log("Server running on %s:%d", host, port);
 });
