6c88848f279a536303c7a06cf5036aebc517d363
Isabek Tashiev <isabek2309@gmail.com>
2020-09-09 21:27:55 +0300

    Chnaged base url

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'domain': 'http://ogrnot.manas.edu.kg/',
+    'domain': 'http://obis.manas.edu.kg/',
     'appKey': "22bcf3f30ee4e3ff384104ddd594d59545cacc13d3b9e7180f3e131537100718"
 };
